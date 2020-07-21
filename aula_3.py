78f16449548a9df43ec4298b36497f18d8abc272
Kevin <kevinsadao@hotmail.com>
2020-07-21 11:10:02 -0300

    Alterado nome do arquivo e aula_3 sobre condicionais e operadores

---
+++ aula_3.py
@@ -0,0 +1,42 @@
+
+a = int(input('Primeiro bimestre: '))
+if a > 10:
+      a = int(input('Você digitou errado. 1º bimestre: '))
+b = int(input('Segundo bimestre: '))
+if b > 10:
+      b = int(input('Você digitou errado. 2º bimestre: '))
+c = int(input('Terceiro bimestre: '))
+if c > 10:
+      c = int(input('Você digitou errado. 3º bimestre: '))
+d = int(input('Quarto bimestre: '))
+if d > 10:
+      d = int(input('Você digitou errado. 4º bimestre: '))
+media = (a + b + c + d)/4
+
+# if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+#       print ('Média: {}'.format(media))
+# else:
+#       print('Informada alguma nota errada')
+
+# a = int(input('Entre com o 1º valor: '))
+# b = int(input('Entre com o 2º valor: '))
+#
+# resto_a = a % 2
+# resto_b = b %2
+#
+# if resto_a == 0 or resto_b == 0:
+#       print('Foi digitado um número par')
+# else:
+#       print('Foi digitado um número ímpar')
+
+# a = int(input('Entre com o 1º valor '))
+# b = int(input('Entre com o 2º valor '))
+# c = int(input('Entre com o 3º valor '))
+#
+# if a > b and a > c:
+#       print ('O maior número é {}'.format(a))
+# elif b > a and b > c:
+#       print('O maior número é {}'.format(b))
+# else:
+#       print('O maior número é {}'.format(c))
+# print ('Final do programa')
