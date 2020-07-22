8c260baaf8547fd75ea4e7176c5b672cc44acde9
Kevin <kevinsadao@hotmail.com>
2020-07-22 14:50:40 -0300

    Aula 5, listas e tuplas

---
+++ aula_5.py
@@ -0,0 +1,63 @@
+# --------------------LISTAS--------------------
+
+lista = [12, 10, 7, 5]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+
+# print(lista_animal[0])
+# for x in lista_animal:
+#     print(x)
+
+# Abaixo código com soma dos valores da lista:
+# soma = 0
+# for x in lista:
+#     print(x)
+#     soma += x
+# print(soma)
+
+# Abaixo código melhorado:
+# print(sum(lista))
+
+# Multiplicando lista
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+
+# Abaixo código mínimo e máximo:
+# Para string, min e max utilizam como base ordem alfabética
+# print(min(lista))
+# print(max(lista))
+# print(min(lista_animal))
+# print(max(lista_animal))
+# Abaixo procurar item específico numa lista
+
+# if 'lobo' in lista_animal:
+#     print('Localizado')
+# else:
+#     print('Não encontrado, será adicionado na lista')
+#     lista_animal.append('lobo')
+# print(lista_animal)
+# lista_animal.pop()
+# lista_animal.pop(1)
+# lista_animal.remove('elefante')
+# print(lista_animal)
+
+# Ordenando listas
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# lista_animal.reverse()
+# print(lista_animal)
+
+# --------------------TUPLAS--------------------
+
+# tupla = (1, 10, 12, 14)
+# print(tupla)
+# print(len(tupla))
+#
+# Convertendo lista em tupla e vice versa
+# tupla_animal = tuple(lista_animal)
+# print(type(tupla_animal))
+# print(tupla_animal)
+# lista_numerica = list(tupla)
+# print(type(lista_numerica))
+# print(lista_numerica)
